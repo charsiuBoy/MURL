eff3e90ca798f1ce7a8ef84a33495752eefe2308
phuc123 <phuc19105@gmail.com>
2024-09-20 02:06:05 -0500

    new 1D simulator

---
+++ README.md
@@ -0,0 +1,18 @@
+#MURL with Prof. Mimi Dai
+
+So I've just add a script lib/1D.py
+
+you can run my script in interact mode:
+`python -i 1D.py`
+
+You can create your own initial condition:
+`x = np.linspace(0, 1, 1000)`
+`init = np.sin(x)`
+
+Then you just pass in my simulator (`Sol` class) like so:
+`sim = Sol(engine, x, init, np.float64(1e-3)) ` (dt = 10^-3)
+
+Then you can see how it evolves:
+`visualizer(x, init, 1)`
+The '1' indicates the video speed. If you want twice the speed, then:
+`visualizer(x, init, 2)`
--- lib/1D.py
+++ lib/1D.py
@@ -1,42 +1,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0,1, 1000, dtype="float64")
+x = np.linspace(-1,1, 100, dtype="float64")
 
-#initial data
-u0 = -x
-'''
-fig, ax = plt.subplots()
-ax.plot(x, u0)
-plt.show()
-'''
-def d_dx(x, u):
-    du = x
+#initial data 
+u0 = -np.arctan(x)
 
-    #return 1D derivative 
-    
-    #at boundary:
-    du[0]=(u[1]-u[0])/(x[1]-x[0])
-    du[len(x)-1] = (u[len(x)-1] - u[len(x)-2])/(x[len(x)-1] - x[len(x)-2])
+def d_dx(x,u):
+    #return np.gradient(u, x)
+    du = np.empty(np.shape(x), dtype="float64")
+    '''
+    du[0] = (u[1]-u[0])/(x[1]-x[0])
+    du[-1] = (u[-1]-u[-2])/(x[-1]-x[-2])
+    '''
+    #derivative at boundary is zero
+    du[0] = 0
+    du[-1] = 0
 
-    for i in range(1,len(x)):
-        du[i]=(u[i]-u[i-1])/(x[i]-x[i-1])
-    return du
-
-du = d_dx(x, u0)
-fig, ax = plt.subplots()
-ax.plot(x, du)
-plt.show()
+    for i in range(1, len(x)-1):
+        du[i] = (u[i+1]-u[i-1])/(x[i+1]-x[i-1])
+    return du 
 
 
-def solver(x,u,t):
+def engine(x,u,dt):
     #return the next step of the system, given current state (u,t), x is space
-    pass
+    du = d_dx(x,u)
+
+    #burgers equation
+    u += - u*du*dt
+
+    return u
+
+class Sol:
+    def __init__(self, engine, x, u, dt):
+        self.engine = engine
+        self.x = x
+        self.init_data = u
+        self.state = u
+        self.dt = dt
 
 
-def integrator(x, u, T):
-    #return the whole evolution of t=0 to t=T, given initial data x lives on space x.
-    pass
+    def __iter__(self):
+        return self
 
+    def __next__(self):
+        self.state = self.engine(self.x, self.state, self.dt)
+        return self.state
+
+
+instance = Sol(engine, x, u0, np.float64(1e-3))
+
+def energy(x, u):
+    return 0.5*np.trapezoid(np.square(u))
+
+def visualizer(x, instance, k=1):
+    fig = plt.figure()
+    for i in range(0, 10000):
+        if i % k == 0: 
+            plt.xlim(-1,1)
+            plt.ylim(-1.5,1.5)
+            state = next(instance)
+            plt.plot(x, state)
+            plt.text(0, 1, "energy"+str(energy(x, state)))
+            plt.pause(1e-6)
+        plt.clf()
+    plt.show()
+
+visualizer(x, instance, 1) 
 
 
